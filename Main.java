f081e6d462da090bc0afe50042a2d9c8d62a2691
ViniciuspDev <viniciuspinheirotecnologia@gmail.com>
2026-03-19 20:22:04 -0300

    Adicionei labels nos enums e corrigi um bug da linha 44 do arquivo main q estava passando um parametro null pra variavel positions e não deixava o jogo ser iniciado

--- Main.java
+++ Main.java
@@ -74,9 +74,13 @@ public class Main {
         for (int i = 0; i < BOARD_LIMIT; i++) {
             spaces.add(new ArrayList<>());
             for (int j = 0; j < BOARD_LIMIT; j++) {
-                var positionConfig = positions.get("%s,%s".formatted(i, j));
+                // Busca a config, se não achar, usa um valor padrão (ex: "0,false")
+                var positionConfig = positions.getOrDefault("%s,%s".formatted(i, j), "0,false");
+
+                // Agora o split nunca pegará um valor nulo
                 var expected = Integer.parseInt(positionConfig.split(",")[0]);
                 var fixed = Boolean.parseBoolean(positionConfig.split(",")[1]);
+
                 var currentSpace = new Space(expected, fixed);
                 spaces.get(i).add(currentSpace);
             }
@@ -125,16 +129,70 @@ public class Main {
             System.out.println("O jogo ainda não foi iniciado");
             return;
         }
+
+        var args = new Object[81];
+        var argPos = 0;
+
+        for (int i = 0; i < BOARD_LIMIT; i++) {
+            for (var col : board.getSpaces()) {
+                args[argPos++] = " " + ((isNull(col.get(i).getActual())) ? " " : col.get(i).getActual());
+            }
+
+        }
+        System.out.println("Seu jogo se encontra da seguinte forma");
+        System.out.printf((BoardTemplate.BOARD_TEMPLATE) + "\n", args);
     }
 
     private static void showCurrentStatus() {
-
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado");
+            return;
+        }
+        System.out.printf("O jogo atualmente se encontra no status %s\n", board.getStatus().getLabel());
+        if (board.hasErrors()) {
+            System.out.println("O jogo contém erros");
+        } else {
+            System.out.println("O jogo não contém erros");
+        }
     }
 
     private static void clearGame() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado");
+            return;
+        }
+
+        System.out.println("Tem certeza de que deseja limpar seu jogo e perder todo o seu progresso?");
+        var confirm = scanner.next();
+        while (!confirm.equalsIgnoreCase("sim") || !confirm.equalsIgnoreCase("não")) {
+            System.out.println("informe 'sim' ou 'não'");
+            confirm = scanner.next();
+
+        }
+
+        if (confirm.equalsIgnoreCase("sim")) {
+            board.reset();
+
+        }
     }
 
     private static void finishGame() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado");
+            return;
+        }
+
+        if (board.gameIsFinished()) {
+            System.out.println("Parabéns, você concluiu o jogo!");
+            showCurrentGame();
+            board = null;
+        } else if (board.hasErrors()) {
+            System.out.println("Seu jogo contém erros, verifique seu board e ajuste");
+
+        } else {
+            System.out.println("Você ainda precisa preencher algum espaço");
+
+        }
     }
 
     private static int runUntilGetValidNumber(final int min, final int max) {
--- model/GameStatusEnum.java
+++ model/GameStatusEnum.java
@@ -2,8 +2,18 @@ package model;
 
 public enum GameStatusEnum {
 
-    NON_STARTED,
-    INCOMPLETE,
-    COMPLETE
+    NON_STARTED("Não iniciado"),
+    INCOMPLETE("Incompleto"),
+    COMPLETE("Completo");
+
+    private String label;
+
+    GameStatusEnum(final String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 
 }
